88c4036bd11e767cbfd44b42b7cfb9be67e61e61
fadyat <fadyat@icloud.com>
2022-05-17 23:07:50 +0300

    template for registration

---
+++ registration/src/main/java/com/team1/registration/RegistrationApplication.java
@@ -0,0 +1,11 @@
+package com.team1.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
+}
