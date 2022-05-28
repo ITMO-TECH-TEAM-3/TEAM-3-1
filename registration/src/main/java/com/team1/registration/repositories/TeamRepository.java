c0095d33c6783b5794e1db2b0d2fb1a194f7ad69
fadyat <fadyat@icloud.com>
2022-05-28 20:59:13 +0300

    rebase team-registration

---
+++ registration/src/main/java/com/team1/registration/repositories/TeamRepository.java
@@ -0,0 +1,10 @@
+package com.team1.registration.repositories;
+
+import com.team1.registration.models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+}
