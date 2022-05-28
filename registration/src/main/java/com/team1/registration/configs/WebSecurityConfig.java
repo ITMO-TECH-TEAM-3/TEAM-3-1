b8d2ee4dcc0fdc4a93d140c6713143224adfe404
Sergo <sergo.elisbar@yandex.ru>
2022-05-29 00:18:43 +0300

    add permissions to /players

--- registration/src/main/java/com/team1/registration/configs/WebSecurityConfig.java
+++ registration/src/main/java/com/team1/registration/configs/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/register").permitAll()
+                    .antMatchers("/", "/register", "/players").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
