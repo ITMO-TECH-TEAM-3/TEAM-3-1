ec184924ca239e650488ba3558ee740f666a9079
fadyat <fadyat@icloud.com>
2022-05-29 00:25:11 +0300

    rebase on user-security

--- registration/src/main/java/com/team1/registration/controllers/navbar/LoginPageController.java
+++ registration/src/main/java/com/team1/registration/controllers/navbar/LoginPageController.java
@@ -1,12 +1,8 @@
 package com.team1.registration.controllers.navbar;
 
-import com.team1.registration.models.User;
-import  org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Map;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginPageController {
+
 }
