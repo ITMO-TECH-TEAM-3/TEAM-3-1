08f4bc9a5a5b2a08b1401ff2d2ca7e82638768f3
lvvnv <levvshk@mail.ru>
2022-05-29 20:15:47 +0300

    feat: add player statistics

---
+++ registration/src/main/java/com/team1/registration/models/PlayerStatistics.java
@@ -0,0 +1,44 @@
+package com.team1.registration.models;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlayerStatistics {
+    private final Player player;
+
+    public PlayerStatistics(Player player){
+        this.player = player;
+    }
+
+    public Integer getTeamId(){
+        return player.getTeam().getId();
+    }
+
+    public double getBalance(){
+        return Double.parseDouble(player.getBalance());
+    }
+
+    public Boolean isBalanceZero(){
+        return getBalance() == 0;
+    }
+
+    public Boolean isBalanceNegative(){
+        return getBalance() < 0;
+    }
+
+    public List<PlayerStatistics> TeamMembersStatistics(){
+        List<PlayerStatistics> list = new LinkedList<PlayerStatistics>();
+        for (Player p:
+             player.getTeam().getPlayers_id()) {
+            list.add(new PlayerStatistics(p));
+        }
+
+        return list;
+    }
+
+    /*
+    public MatchStatistics matchInfo(){
+        todo: extract info from the team's matches
+    }
+    */
+}
