18e36681ae8a1980721841df6423df8e71804b6a
Sergo <sergo.elisbar@yandex.ru>
2022-06-02 22:42:02 +0300

    feat: add passwords encryption & data validation

--- registration/src/main/java/com/team1/registration/services/UserService.java
+++ registration/src/main/java/com/team1/registration/services/UserService.java
@@ -1,20 +1,44 @@
 package com.team1.registration.services;
 
+import com.team1.registration.models.Role;
+import com.team1.registration.models.User;
 import com.team1.registration.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
 
+    private final UserRepository userRepo;
+
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    private UserRepository userRepo;
+    public UserService(UserRepository userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean registerUser(User user){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if(userFromDb != null){
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.AUTHORIZED_USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+        return true;
     }
 }
