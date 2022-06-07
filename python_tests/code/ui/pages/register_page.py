68b22c73f994c2ebbe9fa5bb1a7e3d64f6acb723
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2022-06-08 00:07:39 +0300

    try to run incorrect test

--- python_tests/code/ui/pages/register_page.py
+++ python_tests/code/ui/pages/register_page.py
@@ -6,7 +6,9 @@ from ui.pages.base_page import BasePage
 
 class RegisterPage(BasePage):
     def register(self, username, password):
-        # assert self.browser.current_url == 'http://app:8080/login', f'{self.browser.current_url}'
-        username_input = self.find(RegisterPageLocators.USERNAME_INPUT).send_keys(username)
-        password_input = self.find(RegisterPageLocators.PASSWORD_INPUT).send_keys(password)
-        submit_btn = self.find(RegisterPageLocators.SUBMIT_BUTTON).click()
+#         do incorrect test 
+          assert 1 == 2
+#         assert self.browser.current_url == 'http://app:8080/login', f'{self.browser.current_url}'
+#         username_input = self.find(RegisterPageLocators.USERNAME_INPUT).send_keys(username)
+#         password_input = self.find(RegisterPageLocators.PASSWORD_INPUT).send_keys(password)
+#         submit_btn = self.find(RegisterPageLocators.SUBMIT_BUTTON).click()
