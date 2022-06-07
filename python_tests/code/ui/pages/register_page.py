5e07f1b695866fe122c8da8158b9d5b5b19cdefb
Artyom Fadeyev <70910148+fadyat@users.noreply.github.com>
2022-06-08 00:23:12 +0300

    Update register_page.py

--- python_tests/code/ui/pages/register_page.py
+++ python_tests/code/ui/pages/register_page.py
@@ -6,9 +6,7 @@ from ui.pages.base_page import BasePage
 
 class RegisterPage(BasePage):
     def register(self, username, password):
-#         do incorrect test 
-          assert 1 == 2
 #         assert self.browser.current_url == 'http://app:8080/login', f'{self.browser.current_url}'
-#         username_input = self.find(RegisterPageLocators.USERNAME_INPUT).send_keys(username)
-#         password_input = self.find(RegisterPageLocators.PASSWORD_INPUT).send_keys(password)
-#         submit_btn = self.find(RegisterPageLocators.SUBMIT_BUTTON).click()
+        username_input = self.find(RegisterPageLocators.USERNAME_INPUT).send_keys(username)
+        password_input = self.find(RegisterPageLocators.PASSWORD_INPUT).send_keys(password)
+        submit_btn = self.find(RegisterPageLocators.SUBMIT_BUTTON).click()
