27c4d4734375df81d20541e99bb035d1ab44eead
Sergo <sergo.elisbar@yandex.ru>
2022-06-24 04:29:47 +0300

    uncomment tests

--- python_tests/code/ui/tests/test_ui.py
+++ python_tests/code/ui/tests/test_ui.py
@@ -27,7 +27,6 @@ class TestUI(Database):
             "username": username,
             "password": password
         })
-        main_page.logout()
 
     @pytest.mark.nologin
     def test_register_with_existing_username(self, register_page):
@@ -40,70 +39,69 @@ class TestUI(Database):
         main_page = login_page.login(username, password)
         assert login_page.browser.current_url == f"http://{APP_SERVICE}:{APP_PORT}/", \
             f"actual: {login_page.browser.current_url}, expected: http://{APP_SERVICE}:{APP_PORT}/"
+    @pytest.mark.parametrize("username, password", [("invalid_username", "invalid_password")])
+    @pytest.mark.nologin
+    def test_invalid_login(self, login_page, username, password):
+        login_page.login(username, password)
+        assert login_page.is_element_present(LoginPageLocators.INVALID_DATA_ALERT)
+
+    @pytest.mark.nologin
+    def test_register_with_empty_data(self, register_page):
+        register_page.register("", "")
+        register_page.is_element_present(RegisterPageLocators.EMPTY_USERNAME_ALERT)
+        register_page.is_element_present(RegisterPageLocators.EMPTY_PASSWORD_ALERT)
+
+    @pytest.mark.nologin
+    def test_register_with_too_long_data(self, register_page):
+        data = register_page.fake_invalid_user_data(
+            username=faker.fake_too_long_name(),
+            password=faker.fake_too_long_name())
+        register_page.register(
+            username=data['username'],
+            password=data['password']
+        )
+        register_page.is_element_present(RegisterPageLocators.TOO_LONG_USERNAME_ALERT)
+        register_page.is_element_present(RegisterPageLocators.TOO_LONG_PASSWORD_ALERT)
+
+    def test_logout(self, main_page):
         main_page.logout()
-    # @pytest.mark.parametrize("username, password", [("invalid_username", "invalid_password")])
-    # @pytest.mark.nologin
-    # def test_invalid_login(self, login_page, username, password):
-    #     login_page.login(username, password)
-    #     assert login_page.is_element_present(LoginPageLocators.INVALID_DATA_ALERT)
-    #
-    # @pytest.mark.nologin
-    # def test_register_with_empty_data(self, register_page):
-    #     register_page.register("", "")
-    #     register_page.is_element_present(RegisterPageLocators.EMPTY_USERNAME_ALERT)
-    #     register_page.is_element_present(RegisterPageLocators.EMPTY_PASSWORD_ALERT)
-    #
-    # @pytest.mark.nologin
-    # def test_register_with_too_long_data(self, register_page):
-    #     data = register_page.fake_invalid_user_data(
-    #         username=faker.fake_too_long_name(),
-    #         password=faker.fake_too_long_name())
-    #     register_page.register(
-    #         username=data['username'],
-    #         password=data['password']
-    #     )
-    #     register_page.is_element_present(RegisterPageLocators.TOO_LONG_USERNAME_ALERT)
-    #     register_page.is_element_present(RegisterPageLocators.TOO_LONG_PASSWORD_ALERT)
-    #
-    # def test_logout(self, main_page):
-    #     main_page.logout()
-    #     assert main_page.browser.current_url == f"http://{APP_SERVICE}:{APP_PORT}/login?logout", \
-    #         f"actual: {main_page.browser.current_url}, expected: http://{APP_SERVICE}:{APP_PORT}/login?logout"
-    #     # todo: check active
-    #
-    # @pytest.mark.nologin
-    # @pytest.mark.parametrize("link_name", [
-    #     NavbarLinksNames.HOME,
-    #     NavbarLinksNames.SIGN_IN,
-    #     NavbarLinksNames.SIGN_UP])
-    # def test_go_navbar_link(self, main_page, link_name):
-    #     main_page.go_navbar_link(link_name)
-    #     main_page.navbar_link_transition_is_correct(link_name)
-    #
-    # def test_go_to_profile(self, main_page):
-    #     main_page.go_to_profile()
-    #     assert main_page.browser.current_url == f"http://{APP_SERVICE}:{APP_PORT}/profile"
-    #
-    # @pytest.mark.parametrize("amount", [500])
-    # def test_top_up_balance(self, profile_page, amount):
-    #     # todo: get_active_user instead if active works
-    #     balance_before = self.client.get_users_by_username("test_username")[0].balance
-    #     profile_page.top_up_balance(amount)
-    #     assert self.client.get_users_by_username("test_username")[0].balance - balance_before == amount
-    #
-    # @pytest.mark.parametrize("profile_option", [ProfileOptionsNames.TOP_UP,
-    #                                             ProfileOptionsNames.PLAYERS_PAGE,
-    #                                             ProfileOptionsNames.TEAMS_PAGE])
-    # def test_go_to_profile_options(self, profile_page, profile_option):
-    #     profile_page.go_to_profile_options(profile_option)
-    #     profile_page.profile_link_transition_is_correct(profile_option)
-    #
-    # @pytest.mark.parametrize("name", ["aboba"])
-    # def test_create_player(self, profile_page, name):
-    #     profile_page.create_player(name)
-    #     assert len(self.client.get_players_by_name(name))
-    #
-    # @pytest.mark.parametrize("name", ["aboba"])
-    # def test_create_team(self, profile_page, name):
-    #     profile_page.create_team(name)
-    #     assert len(self.client.get_teams_by_name(name))
+        assert main_page.browser.current_url == f"http://{APP_SERVICE}:{APP_PORT}/login?logout", \
+            f"actual: {main_page.browser.current_url}, expected: http://{APP_SERVICE}:{APP_PORT}/login?logout"
+        # todo: check active
+
+    @pytest.mark.nologin
+    @pytest.mark.parametrize("link_name", [
+        NavbarLinksNames.HOME,
+        NavbarLinksNames.SIGN_IN,
+        NavbarLinksNames.SIGN_UP])
+    def test_go_navbar_link(self, main_page, link_name):
+        main_page.go_navbar_link(link_name)
+        main_page.navbar_link_transition_is_correct(link_name)
+
+    def test_go_to_profile(self, main_page):
+        main_page.go_to_profile()
+        assert main_page.browser.current_url == f"http://{APP_SERVICE}:{APP_PORT}/profile"
+
+    @pytest.mark.parametrize("amount", [500])
+    def test_top_up_balance(self, profile_page, amount):
+        # todo: get_active_user instead if active works
+        balance_before = self.client.get_users_by_username("test_username")[0].balance
+        profile_page.top_up_balance(amount)
+        assert self.client.get_users_by_username("test_username")[0].balance - balance_before == amount
+
+    @pytest.mark.parametrize("profile_option", [ProfileOptionsNames.TOP_UP,
+                                                ProfileOptionsNames.PLAYERS_PAGE,
+                                                ProfileOptionsNames.TEAMS_PAGE])
+    def test_go_to_profile_options(self, profile_page, profile_option):
+        profile_page.go_to_profile_options(profile_option)
+        profile_page.profile_link_transition_is_correct(profile_option)
+
+    @pytest.mark.parametrize("name", ["aboba"])
+    def test_create_player(self, profile_page, name):
+        profile_page.create_player(name)
+        assert len(self.client.get_players_by_name(name))
+
+    @pytest.mark.parametrize("name", ["aboba"])
+    def test_create_team(self, profile_page, name):
+        profile_page.create_team(name)
+        assert len(self.client.get_teams_by_name(name))
